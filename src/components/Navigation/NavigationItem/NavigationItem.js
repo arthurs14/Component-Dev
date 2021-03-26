3827f48555baa45145938af4dab5cf99773f6a92
arthurs14 <arthurcs426@gmail.com>
2021-03-25 17:42:15 -0700

    working on styling the navigation links

--- src/components/Navigation/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItem/NavigationItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './NavigationItem.module.css';
+
 const navigationItem = ({ link, title }) => (
   <li>
     <NavLink to={link} exact>{title}</NavLink>
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import NavigationItem from '../NavigationItem/NavigationItem';
+import classes from './NavigationItems.module.css';
 
 const navigationItems = () => (
-  <ul>
-    <NavigationItem title="Industries" link="/industries" />
-    <NavigationItem title="Services" link="/services" />
-    <NavigationItem title="About-Us" link="/about-us" />
-  </ul>
+  <div className={classes.links}>
+    <ul className={classes.ul}>
+      <NavigationItem title="Industries" link="/industries" />
+      <NavigationItem title="Services" link="/services" />
+      <NavigationItem title="About-Us" link="/about-us" />
+    </ul>
+  </div>
 );
 
 export default navigationItems;
